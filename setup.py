8a39496a115415f4b48faaf3390aa759651732e8
Pawel Kierski <pkierski@gog.com>
2025-09-18 16:10:57 +0200

    Add missing pyproject.toml file

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="galaxy.plugin.api",
-    version="0.70",
-    description="GOG Galaxy Integrations Python API",
-    author='Galaxy team',
-    author_email='galaxy@gog.com',
-    packages=find_packages("src"),
-    package_dir={'': 'src'},
-    python_requires="~=3.13.0",  # This package working with Python 3.13.x embedded in GOG Galaxy 2.0
-    install_requires=[
-        "aiohttp>=3.12.15",
-        "certifi>=2025.8.3",
-        "psutil>=5.6.6; sys_platform == 'darwin'"
-    ]
-)
+setup()
